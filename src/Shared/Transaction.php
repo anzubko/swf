47f75ea16e46c425e5219bd4d47576e8c586c5e4
Andrian Zubko <ondr@mail.ru>
2024-10-11 21:43:36 +0300

    dev

--- src/Shared/Transaction.php
+++ src/Shared/Transaction.php
@@ -2,40 +2,15 @@
 
 namespace App\Shared;
 
-use App\Config\TransactionConfig;
-use SWF\Exception\DatabaserException;
-use SWF\Interface\DatabaserInterface;
-use SWF\TransactionDeclaration;
 use SWF\TransactionRunner;
-use Throwable;
-use function count;
 
+/**
+ * @mixin TransactionRunner
+ */
 class Transaction
 {
-    /**
-     * @var TransactionDeclaration[] $declarations
-     */
-    private array $declarations = [];
-
-    public function with(DatabaserInterface $db, ?string $isolation = null, string ...$states): static
+    public static function getInstance(): TransactionRunner
     {
-        $this->declarations[] = new TransactionDeclaration($db, $isolation, $states);
-
-        return $this;
-    }
-
-    /**
-     * @throws DatabaserException
-     * @throws Throwable
-     */
-    public function run(callable $body, ?int $retries = null): void
-    {
-        if (count($this->declarations) === 0) {
-            $this->declarations[] = new TransactionDeclaration(i(Db::class));
-        }
-
-        i(TransactionRunner::class)->run($body, $this->declarations, $retries ?? i(TransactionConfig::class)->retries);
-
-        $this->declarations = [];
+        return new TransactionRunner(i(Db::class));
     }
 }
